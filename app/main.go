fc89ca24100cdd5e65cd528669b6315d74ac45dc
Joel Atherley <joelnatherley@gmail.com>
2020-05-25 18:32:18 +0200

    Add Rubens elastic stack app

---
+++ app/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/rubencougil/geekshubs/elastic/app/user"
+	"github.com/sirupsen/logrus"
+	ginlogrus "github.com/toorop/gin-logrus"
+	"go.elastic.co/apm/module/apmgin"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+
+	logger := Logger()
+	db := Database(logger)
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+
+	r.Use(ginlogrus.Logger(logger), gin.Recovery())
+	r.Use(apmgin.Middleware(r))
+
+	r.GET("/", func(c *gin.Context) { c.JSON(200, gin.H{}) })
+	r.POST("/create", user.CreateUserHandler(logger, user.NewUserStore(db, logger)))
+
+	_ = r.Run(":80")
+}
+
+func Database(logger *logrus.Logger) *sqlx.DB {
+	db, err := sqlx.Connect("mysql", "user:password@(db:3306)/db")
+	if err != nil {
+		logger.Fatalf("Cannot connect to the database: %v", err)
+	}
+	return db
+}
+
+func Logger() *logrus.Logger {
+	logger := logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{}
+	logger.SetLevel(logrus.DebugLevel)
+	log.SetOutput(logger.Writer())
+	logger.SetOutput(io.MultiWriter(os.Stdout))
+	return logger
+}
---
+++ app/user/user.go
@@ -0,0 +1,69 @@
+package user
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+)
+
+type User struct {
+	Name  string
+	Email string
+}
+
+type randomUser struct {
+	Results []struct {
+		Name struct {
+			First string `json:"first"`
+			Last  string `json:"last"`
+		} `json:"name"`
+		Email string `json:"email"`
+	} `json:"results"`
+}
+
+func CreateUserHandler(logger *logrus.Logger, store Store) (fn gin.HandlerFunc) {
+	fn = func(c *gin.Context) {
+		u, err := generate()
+		if err != nil {
+			logger.WithFields(logrus.Fields{"error": err}).Error("Error handling Create User")
+			c.JSON(500, gin.H{"message": "cannot generate a new random user"})
+			return
+		}
+		if err := store.Save(u); err != nil {
+			logger.WithFields(logrus.Fields{"error": err}).Error("Error handling Create User")
+			c.JSON(500, gin.H{"message": "cannot create new user"})
+			return
+		}
+		logger.WithFields(logrus.Fields{"name": u.Name, "email": u.Email}).Info("New user has been created")
+		c.JSON(200, gin.H{"message": "ok",})
+	}
+
+	return
+}
+
+func generate() (user *User, err error) {
+
+	res, err := http.Get("http://randomuser:3000/api/")
+
+	if err != nil {
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("Random User is not working: %d", res.StatusCode))
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	ru := randomUser{}
+	_ = json.Unmarshal(body, &ru)
+
+	return &User{
+		Name:  ru.Results[0].Name.First + " " + ru.Results[0].Name.Last,
+		Email: ru.Results[0].Email,
+	}, nil
+}
